8e0626d19eb17877e0de7b3bcd7054017653273a
Cthompsonlbi <chad.thompson@Qorvo.com>
2021-11-04 18:45:00 -0400

    completed 3.5.5

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,12 @@ candidate_options = []
 #Declare the empty dictionary
 candidate_votes = {}
 
+#Winning Candidate and Winning Count Trakcer
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
+
 #open the election results and read the file.
 with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
@@ -52,6 +58,37 @@ for candidate_name in candidate_votes:
     #4. Print the candidate name and percentage of votes.
     print(f"{candidate_name}: received {vote_percentage: .2f}% of the vote.")
 
+#Determine the percentage of votes for each candidate by looping through the counts
+#Iterate through the candidate list.
+for candidate_name in candidate_votes:
+    #retrieve vote count of a candidate.
+    votes = candidate_votes[candidate_name]
+    #calculate the percentages of votes.
+    vote_percentage = float(votes) / float(total_votes) * 100
+
+    #To do: print out each candidates name, vote count, and percentage of votes to the terminal
+    print(f"{candidate_name}: {vote_percentage: .1f}%({votes:,})\n")
+
+    #Determine winning vote count and candidate
+    #Determine if the votes is greater than the winning count.
+    if (votes> winning_count) and (vote_percentage > winning_percentage):
+        #If true then set winning_count = votes and winning_percentage= vote_percentage
+        winning_count = votes
+        winning_percentage = vote_percentage
+        #And, set teh winning_candidate equal to the candidate's name.
+        winning_candidate = candidate_name
+
+# winning candidate summary
+winning_candidate_summary = (
+    f"------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage: .1f}%\n"
+    f"------------------------\n")
+print(winning_candidate_summary)
+
+
+    # To do: print out the winning candidate, vote count and percentage to terminal.
 
 #Print the candidate vote dictionary.
 #print(candidate_votes)
